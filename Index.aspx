a9df4eec2b0c12b02baccca1adf60eb024824e74
Amninder-Chahal <amninderchahhal@gmail.com>
2017-01-21 17:01:55 -0500

    Add files via upload

---
+++ Index.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Index.aspx.cs" Inherits="Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Play Blackjack</title>
+    <link href="Stylesheet.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <div class="content">
+    <header>
+        <p class="headerText">BlackJack</p>
+    </header>
+    <form id="form1" runat="server">
+     <p class="text para"><asp:Label ID="labelPara" runat="server" Text="Click Deal to start playing the game"></asp:Label></p><br />
+   <div class="textContainer">
+    <div class="table">
+        <div class="row">
+            <div class="column col-1">
+                <p class="text"><asp:Label ID="labelUserScore" runat="server" Text=""> </asp:Label></p>
+            </div>
+            <div class="column col-2">
+                <p class="text"><asp:Label ID="labelDealerScore" runat="server" Text=""> </asp:Label></p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="column col-2-1">
+                <p class="text"><asp:Label ID="labelResult" runat="server" Text=""></asp:Label></p>
+            </div>
+       </div>
+    </div>
+    </div>
+    <div class="buttonsDiv">        
+        <asp:Button CssClass="button" ID="deal" runat="server" Text="Deal" OnClick="deal_Click" />
+        <asp:Button CssClass="button" ID="hit" runat="server" Text="Hit" OnClick="hit_Click" />
+        <asp:Button CssClass="button" ID="pass" runat="server" Text="Pass" OnClick="pass_Click" />
+        <asp:Button CssClass="button" ID="endGame" runat="server" Text="Restart" OnClick="endGame_Click" />
+    </div>
+    </form>
+    </div>
+</body>
+</html>
---
+++ Index.aspx.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Index : System.Web.UI.Page
+{
+    public static int userScore, dealerScore;
+    Random rnd = new Random();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+    }
+ 
+    protected void Deal()
+    {
+        int card_1 , card_2;
+        card_1 = rnd.Next(1, 11);
+        card_2 = rnd.Next(1, 11);
+        userScore = card_1 + card_2;
+        labelUserScore.Text = Convert.ToString("User Score: " + userScore);
+    }
+    protected void Hit()
+    {
+        int hitCard ;
+        hitCard = rnd.Next(1, 11);
+        userScore = userScore + hitCard;
+        labelUserScore.Text = Convert.ToString("User Score: " + userScore);
+
+        if (userScore > 21)
+        {
+            labelResult.Text = "Dealer wins!";
+            userScore = 0;
+            dealerScore = 0;
+        }
+    }
+    protected void Pass()
+    {
+        int passCard;
+        while (dealerScore < 17)
+        {
+            passCard = rnd.Next(1, 11);
+            dealerScore = dealerScore + passCard;
+            labelDealerScore.Text = Convert.ToString("Dealer Score: " + dealerScore);
+            if (dealerScore > 21)
+            {
+                labelResult.Text = "Player wins!";
+                userScore = 0;
+                dealerScore = 0;
+            }
+        }
+
+        if (userScore > dealerScore)
+        {
+            labelResult.Text = "Player Wins!";
+        }
+        else if (userScore == dealerScore)
+        {
+            labelResult.Text = "Tie Game!";
+        }
+        else if (userScore < dealerScore)
+        {
+            labelResult.Text = "Dealer Wins!";
+        }
+
+        userScore = 0;
+        dealerScore = 0;
+    }
+    protected void end_Game()
+    {
+        labelDealerScore.Text = "";
+        labelResult.Text = "";
+        labelUserScore.Text = "";
+
+        userScore = 0;
+        dealerScore = 0;
+    }
+
+    protected void deal_Click(object sender, EventArgs e)
+    {
+        userScore = 0;
+        dealerScore = 0;
+        end_Game();
+        labelPara.Text = "";
+        Deal();
+    }
+
+    protected void hit_Click(object sender, EventArgs e)
+    {
+        labelPara.Text = "";
+        Hit();
+    }
+
+    protected void pass_Click(object sender, EventArgs e)
+    {
+        labelPara.Text = "";
+        Pass();
+    }
+
+    protected void endGame_Click(object sender, EventArgs e)
+    {
+        labelPara.Text = "Click Deal to start playing the game";
+        end_Game();
+    }
+}
